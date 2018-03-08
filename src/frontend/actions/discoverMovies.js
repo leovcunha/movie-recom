512e60fbfff62ce68bc2ed99604780c71cb6ae83
leovcunha <lvcunha@gmail.com>
2018-03-08 23:31:43 +0000

    css work

--- src/frontend/actions/discoverMovies.js
+++ src/frontend/actions/discoverMovies.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 const APIKEY = 'api_key=9624561704e52e84ae59cd0147eb662d';
 
-export const discoverMovies = function () {
-    return axios.get(`https://api.themoviedb.org/3/discover/movie?${APIKEY}&language=en-US&page=1`);   
+export const discoverMovies = function (pg = 1) {
+    return axios.get(`https://api.themoviedb.org/3/discover/movie?${APIKEY}&language=en-US&page={pg}&sort_by=popularity.desc`);   
 };
--- src/frontend/app.js
+++ src/frontend/app.js
@@ -8,8 +8,12 @@ import MovieTable from './components/MovieTable';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.pageHandler = this.pageHandler.bind(this);
+    
     this.state = {
         isLoading: false,
+        moviePage: 1,
         movieList: {
             results: []
         }
@@ -17,16 +21,31 @@ class App extends React.Component {
     };
   }
 componentDidMount() {
-    this.setState({isLoading: true})
+    this.setState({isLoading: true});
     discoverMovies().then(res => {
         const movieList = res.data;
         this.setState({ movieList, isLoading: false });
     });   
 }
 
+pageHandler(increm) {
+    let mp = this.state.moviePage;
+    if increm {
+        mp =  mp + 1;
+    } 
+    else if (mp > 1) {
+        mp = mp - 1;
+    }
+
+    this.setState({
+        moviePage: mp
+    }); 
+    
+}
+
 render() {
 
-    console.log(this.state.movieList)
+    console.log(this.state.movieList);
     
     return (
       <div>
