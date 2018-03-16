9171c30f7eac615f5a9c9130eb4a2d0987359d9f
leovcunha <lvcunha@gmail.com>
2018-03-16 18:16:31 -0300

    working on user login /security

--- src/main/java/com/movierec/SpectatorRepository.java
+++ src/main/java/com/movierec/SpectatorRepository.java
@@ -3,5 +3,7 @@ package com.movierec;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SpectatorRepository extends CrudRepository<Spectator, Long> {
+	
+	Spectator findByUserName(String userName);
 
 }
