4f42f71d42b7750d95b4583dfd3d9b8baddd943b
leovcunha <lvcunha@gmail.com>
2024-10-24 15:14:55 +0100

    chore: configure new webpack version and babel transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,48 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/frontend/app.js',
+    entry: "./src/frontend/app.js",
 
-  output: {
-    path: path.resolve(__dirname, 'src/main/resources/static/'),
-    filename: 'built/app-bundle.js',
-    publicPath: '/'
-  }, 
-
-  plugins: [
-
-    new HtmlWebpackPlugin({ //dev
-        hash: true,
-        template: 'src/main/resources/templates/template.html',
-    }),
-    new HtmlWebpackPlugin({ //production
-        hash: false,
-        template: 'src/main/resources/templates/template.html',
-        filename: path.join(__dirname, 'src/main/resources/templates/index.html')
-    })
-  ],
-
-  devServer: {
-
-    contentBase: './src/main/resources/static/',
-
-    proxy: {  
-     '/api/*': {
-       target: 'http://localhost:8080',
-       secure: false,
-       publicPath: "/"
-     
-      },
+    output: {
+        path: path.resolve(__dirname, "src/main/resources/static/"),
+        filename: "built/app-bundle.js",
+        publicPath: "/",
     },
-    
-   },
-   module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'react']
-        }
-      }
-    }
-  ]
-}
 
-  
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: "src/main/index.html",
+            filename: "index.html",
+        }),
+    ],
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "src/main/resources/static/"),
+        },
+        proxy: {
+            "/api/*": {
+                target: "http://localhost:8080",
+                secure: false,
+                publicPath: "/",
+            },
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
