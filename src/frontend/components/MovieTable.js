5a74db99fd795277c215fd79bd2590e65bdcd2f4
leovcunha <lvcunha@gmail.com>
2024-10-26 13:45:56 +0100

    refactor: update components to functional react

--- src/frontend/components/MovieTable.js
+++ src/frontend/components/MovieTable.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import MovieHolder from './MovieHolder';
+import React, { useState, useEffect } from "react";
+import MovieHolder from "./MovieHolder";
+import { discoverMovies } from "../actions/discoverMovies";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-export default class MovieTable extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    listMovies() {
-        return (
-          this.props.movies.map((movieData) => {
-            return <MovieHolder key={movieData.id} movie={movieData}/>             
-          })
-        )
-    }
-    
-    render() {
-        
-        return (
-            <div className="movietable">
-                <i className="material-icons arrow-left" onClick={() => 
-                    this.props.pagehandler(false)}>chevron_left</i>       
-                <div className="movielist">
-                    {this.listMovies()}
-                </div>
-                <i className="material-icons arrow-right" onClick={() => 
-                    this.props.pagehandler(true)}>chevron_right</i>
+const MovieTable = ({ movies, pagehandler, currentPage }) => {
+    const handleClick = (e, direction) => {
+        e.preventDefault();
+        console.log(`${direction} arrow clicked`);
+        console.log("Current page:", currentPage);
+        pagehandler(direction === "right");
+    };
+
+    return (
+        <div className="movietable">
+            <div
+                className="arrow-left"
+                onClick={(e) => handleClick(e, "left")}
+                style={{ cursor: "pointer" }}
+            >
+                <FontAwesomeIcon icon={faChevronLeft} size="2x" />
             </div>
-        );
-    }
-    
-    
-    
-}
+            <div className="movielist">
+                {movies?.map((movieData) => (
+                    <MovieHolder key={movieData.id} movie={movieData} />
+                ))}
+            </div>
+            <div
+                className="arrow-right"
+                onClick={(e) => handleClick(e, "right")}
+                style={{ cursor: "pointer" }}
+            >
+                <FontAwesomeIcon icon={faChevronRight} size="2x" />
+            </div>
+        </div>
+    );
+};
+
+export default MovieTable;
