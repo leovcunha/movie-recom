08a2b8ffa34bb841d1b85dc9d5c2bd33c7a1fcbb
leovcunha <lvcunha@gmail.com>
2024-10-27 10:23:45 +0000

    refactor: remove login no longer used

--- src/frontend/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import UserReducer from './reducer_user';
-
-
-const rootReducer = combineReducers({
-    user: UserReducer,
-});
-
-export default rootReducer;
